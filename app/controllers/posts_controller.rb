20b6c9c90de7de83d02609ea12641802fa0b780b
Samanthaponce5 <samanthaponce5@gmail.com>
2020-04-19 02:49:01 -0400

    post-view-only-following

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
     def index
-       @posts = Post.all
+        @posts = Post.of_followed_accounts(current_user.following).order('created_at DESC')
   
     end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
     belongs_to :account
     has_many  :comments
     has_one_attached:image 
+
+    scope :of_followed_accounts, -> (following_accounts) { where account_id: following_accounts }
 end
